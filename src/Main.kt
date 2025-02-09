8b630814937746ee9782ecc345693350004f09d7
Belen Estrella Hervas Pardo <belen.hervas01@gmail.com>
2025-02-09 13:52:19 -0500

    kotlin

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    var name = "my name is pino"
+    name = "Pedro"
+    //values with "val" cannot be changed
+    val age = 13
+
+    println("Here is my name and age:")
+    println(name)
+    println(age)
+}
